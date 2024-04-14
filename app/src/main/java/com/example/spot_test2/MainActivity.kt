4f74828d551dcde5758c355acc707a953c0462cd
gioshiro808 <albertuspoetra@gmail.com>
2024-04-14 23:52:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/spot_test2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.spot_test2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btn = findViewById<Button>(R.id.btn_regis);
+        btn.setOnClickListener {
+            Toast.makeText(applicationContext, "Tombol berhasil bekerja", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
